bb9eb75ae9e4eae08a99010526c1e3ad562eedd5
Harshitha <nharshitha728@gmail.com>
2026-02-14 09:34:00 +0530

    Update SimpleTest.java

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,10 +1,20 @@
+package com.example;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class SimpleTest {
+class CalculatorTest {
+    Calculator calc = new Calculator();
+
     @Test
-    void testLogic() {
-        int sum = 10 + 10;
-        assertEquals(20, sum, "Math should work!");
+    void testAddition() {
+        // We expect 10 + 5 to be 15
+        assertEquals(15, calc.add(10, 5), "Addition logic is broken!");
+    }
+
+    @Test
+    void testMultiplication() {
+        // We expect 2 * 3 to be 6
+        assertEquals(6, calc.multiply(2, 3));
     }
 }
