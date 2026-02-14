79cca5d5494652d223400dbbb5cb76b7aba72a7c
Harshitha <nharshitha728@gmail.com>
2026-02-14 09:33:23 +0530

    Add Calculator class with add and multiply methods

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Calculator {
+    // The simple logic we want to verify
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+}
